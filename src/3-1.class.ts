fcf9700074bedb0cdbdadcb99580439e934c80ef
rootnure <nur.diu.2791@gmail.com>
2024-06-13 22:23:46 +0600

    How to declare a class, declare property & method in class, declare property type, create instance from class, public keyword and shortcut in TS for public keyword

--- src/3-1.class.ts
+++ src/3-1.class.ts
@@ -1,14 +1,16 @@
 {
   // oop - class
   class Animal {
-    name: string;
-    species: string;
-    sound: string;
+    // public name: string;
+    // public species: string;
+    // public sound: string;
 
-    constructor(name: string, species: string, sound: string) {
-      this.name = name;
-      this.species = species;
-      this.sound = sound;
+    /* parameter properties */
+
+    constructor(public name: string, public species: string, public sound: string) {
+      // this.name = name;
+      // this.species = species;
+      // this.sound = sound;
     }
 
     makeSound() {
