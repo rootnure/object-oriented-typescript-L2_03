af0b717a8acec40afc07a344fd7ceea35378464e
rootnure <nur.diu.2791@gmail.com>
2024-06-14 14:20:58 +0600

    Abstraction --> OOP (using abstract class). And abstract note added to README.md

--- src/3-9.abstraction-oop.ts
+++ src/3-9.abstraction-oop.ts
@@ -1,9 +1,12 @@
 {
-  // abstraction --> oop
-  // 1. interface
-  // 2. abstract
+  /** abstraction --> oop
+    *  - 1. interface [leader interface]
+    *  - 2. abstract class [leader class]
+  */
 
-  /* ------------ idea ----------- */
+  /********** interface ***********/
+
+  /* ------------ idea / leader interface ----------- */
   interface Vehicle1 {
     startEngine(): void;
     stopEngine(): void;
@@ -11,7 +14,44 @@
   }
 
   /* --------- real implementation ---------- */
-  class Car implements Vehicle1 {
+  class Car1 implements Vehicle1 { // must need to use implements keyword for abstraction using interface
+    startEngine(): void {
+      console.log('Engine Started');
+    }
+    stopEngine(): void {
+      console.log('Engine Stop');
+    }
+    move(): void {
+      console.log('Car is moving');
+    }
+    test(): void {
+      console.log('Extra method');
+    }
+  }
+
+  // creating instance of Car
+  const toyotaCar = new Car1();
+  // calling methods of Car instance
+  toyotaCar.startEngine();
+  toyotaCar.stopEngine();
+  toyotaCar.move();
+  toyotaCar.test();
+
+  console.log();
+
+
+
+  /*************** abstract class ************/
+
+  /* ------------ idea / leader class ------------- */
+  abstract class Vehicle2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+  }
+
+  /* -------- implementation -------- */
+  class Car2 extends Vehicle2 {
     startEngine(): void {
       console.log('Engine Started');
     }
@@ -27,12 +67,13 @@
   }
 
 
-  const toyotaCar = new Car();
-
-  toyotaCar.startEngine();
-  toyotaCar.stopEngine();
-  toyotaCar.move();
-  toyotaCar.test();
+  // creating instance of Car
+  const hondaCar = new Car2();
+  // calling methods of Car instance
+  hondaCar.startEngine();
+  hondaCar.stopEngine();
+  hondaCar.move();
+  hondaCar.test();
 
 
 
